107034723adcd0af2b300f80e4e7bbf2f2e2d30c
Vritika Singla <vritikabnl242@gmail.com>
2018-07-13 13:41:50 +0000

    add app.js

--- v1/app.js
+++ v1/app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v4");
+// mongoose.connect("mongodb://localhost/yelp_camp_v4");
+mongoose.connect("mongodb://vritika:vritika13100@ds235401.mlab.com:35401/yelpcampapp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
